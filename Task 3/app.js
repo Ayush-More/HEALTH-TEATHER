c1169a4b6b6640bf2981fdbf348282a195bb4fe5
Ayush More <129107864+Ayush-More@users.noreply.github.com>
2024-01-21 11:52:30 +0530

    #4
    express validator

--- Task 3/app.js
+++ Task 3/app.js
@@ -50,6 +50,7 @@ app.post(
   "/staffEntry",
   validate([
     body("staffId")
+      .optional()
       .trim()
       .notEmpty()
       .withMessage("Staff ID is required")
@@ -57,7 +58,6 @@ app.post(
       .isLength({ max: 255 })
       .withMessage("Staff ID must be at most 255 characters"),
 
-    // Validate firstName
     body("firstName")
       .trim()
       .notEmpty()
@@ -67,8 +67,8 @@ app.post(
       .withMessage("First name must be at most 255 characters")
       .custom(isAlphabetic),
 
-    // Validate lastName
     body("lastName")
+      .optional()
       .trim()
       .notEmpty()
       .withMessage("Last name is required")
@@ -77,35 +77,36 @@ app.post(
       .withMessage("Last name must be at most 255 characters")
       .custom(isAlphabetic),
 
-    // Validate specialization
     body("specialization")
+      .optional()
       .trim()
       .escape()
       .isLength({ max: 50 })
       .withMessage("Specialization must be at most 50 characters"),
 
-    // Validate isDoctor
-    body("isDoctor").isBoolean().withMessage("Invalid value for isDoctor"),
+    body("isDoctor")
+      .optional()
+      .isBoolean()
+      .withMessage("Invalid value for isDoctor"),
 
-    // Validate age
     body("age")
+      .optional()
       .isInt({ min: 1, max: 100 })
       .withMessage("Age must be between 1 and 100"),
 
-    // Validate birthday
     body("birthday")
+      .optional()
       .isISO8601()
       .toDate()
       .withMessage("Invalid date format for birthday"),
 
-    // Validate gender
     body("gender")
+      .optional()
       .trim()
       .escape()
       .isLength({ max: 10 })
       .withMessage("Gender must be at most 10 characters"),
 
-    // Validate mobile
     body("mobile")
       .trim()
       .notEmpty()
@@ -116,22 +117,22 @@ app.post(
       .isMobilePhone("any", { strictMode: false })
       .withMessage("Invalid mobile number"),
 
-    // Validate countryCode
     body("countryCode")
+      .optional()
       .trim()
       .escape()
       .isLength({ max: 10 })
       .withMessage("Country code must be at most 10 characters"),
 
-    // Validate whatsapp
     body("whatsapp")
+      .optional()
       .trim()
       .escape()
       .isLength({ max: 255 })
       .withMessage("WhatsApp must be at most 255 characters"),
 
-    // Validate email
     body("email")
+      .optional()
       .trim()
       .escape()
       .isEmail()
@@ -139,144 +140,148 @@ app.post(
       .isLength({ max: 100 })
       .withMessage("Email must be at most 100 characters"),
 
-    // Validate address.house
     body("address.house")
+      .optional()
       .trim()
       .escape()
       .isLength({ max: 255 })
       .withMessage("House must be at most 255 characters"),
 
-    // Validate address.street
     body("address.street")
+      .optional()
       .trim()
       .escape()
       .isLength({ max: 1000 })
       .withMessage("Street must be at most 1000 characters"),
 
-    // Validate address.landmarks
     body("address.landmarks")
+      .optional()
       .trim()
       .escape()
       .isLength({ max: 1000 })
       .withMessage("Landmarks must be at most 1000 characters"),
 
-    // Validate address.city
     body("address.city")
+      .optional()
       .trim()
       .escape()
       .isLength({ max: 500 })
       .withMessage("City must be at most 500 characters"),
 
-    // Validate address.pincode
     body("address.pincode")
+      .optional()
       .trim()
       .escape()
       .isLength({ max: 50 })
       .withMessage("Pincode must be at most 50 characters"),
 
-    // Validate documentType
     body("documentType")
+      .optional()
       .trim()
       .escape()
       .isLength({ max: 100 })
       .withMessage("Document type must be at most 100 characters"),
 
-    // Validate documentNumber
     body("documentNumber")
+      .optional()
       .trim()
       .escape()
       .isLength({ max: 100 })
       .withMessage("Document number must be at most 100 characters"),
 
-    // Validate upiId
     body("upiId")
+      .optional()
       .trim()
       .escape()
       .isLength({ max: 100 })
       .withMessage("UPI ID must be at most 100 characters"),
 
-    // Validate bankName
     body("bankName")
+      .optional()
       .trim()
       .escape()
       .isLength({ max: 100 })
       .withMessage("Bank name must be at most 100 characters"),
 
-    // Validate accountName
     body("accountName")
+      .optional()
       .trim()
       .escape()
       .isLength({ max: 255 })
       .withMessage("Account name must be at most 255 characters"),
 
-    // Validate accountNo
     body("accountNo")
+      .optional()
       .trim()
       .escape()
       .isLength({ max: 100 })
       .withMessage("Account number must be at most 100 characters"),
 
-    // Validate ifsc
     body("ifsc")
+      .optional()
       .trim()
       .escape()
       .isLength({ max: 50 })
       .withMessage("IFSC must be at most 50 characters"),
 
-    // Validate isAdmin
-    body("isAdmin").isBoolean().withMessage("Invalid value for isAdmin"),
+    body("isAdmin")
+      .optional()
+      .isBoolean()
+      .withMessage("Invalid value for isAdmin"),
 
-    // Validate created.on
     body("created.on")
+      .optional()
       .isISO8601()
       .toDate()
       .withMessage("Invalid date format for created.on"),
 
-    // Validate created.by.id
     body("created.by.id")
+      .optional()
       .trim()
       .escape()
       .isLength({ max: 255 })
       .withMessage("Created by ID must be at most 255 characters"),
 
-    // Validate created.by.name
     body("created.by.name")
+      .optional()
       .trim()
       .escape()
       .isLength({ max: 255 })
       .withMessage("Created by name must be at most 255 characters"),
 
-    // Validate modified.on
     body("modified.on")
+      .optional()
       .isISO8601()
       .toDate()
       .withMessage("Invalid date format for modified.on"),
 
-    // Validate modified.by.id
     body("modified.by.id")
+      .optional()
       .trim()
       .escape()
       .isLength({ max: 255 })
       .withMessage("Modified by ID must be at most 255 characters"),
 
-    // Validate modified.by.name
     body("modified.by.name")
+      .optional()
       .trim()
       .escape()
       .isLength({ max: 255 })
       .withMessage("Modified by name must be at most 255 characters"),
 
-    // Validate profilePic
-    body("profilePic").trim().escape(),
+    body("profilePic").optional().trim().escape(),
 
-    // Validate documents
-    body("documents").isArray().withMessage("Documents must be an array"),
+    body("documents")
+      .optional()
+      .isArray()
+      .withMessage("Documents must be an array"),
 
-    // Validate deleted
-    body("deleted").isBoolean().withMessage("Invalid value for deleted"),
+    body("deleted")
+      .optional()
+      .isBoolean()
+      .withMessage("Invalid value for deleted"),
 
-    // Validate user
-    body("user").isMongoId().withMessage("Invalid user ID"),
+    body("user").optional().isMongoId().withMessage("Invalid user ID"),
   ]),
   async (req, res) => {
     try {
